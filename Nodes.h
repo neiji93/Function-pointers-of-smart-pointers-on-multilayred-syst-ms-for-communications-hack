b8ff04e8ec77aa214be58a3c294ac2104ba2b4ea
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-14 11:34:22 +0100

    Create Nodes.h

---
+++ Nodes.h
@@ -0,0 +1,14 @@
+//it is clear that memory leaks can be overcome
+//but it is not clear if small programs can hold big datas.
+//so, lets keep the kernel datas and classes safe !
+
+class NoOvercomeMixedServices
+{
+private:
+  double thresholdbase...
+  double extremeThresholdOfLayerSizeInMemory=....
+
+public:
+  //our system may check some.rhings with ai
+  double traceGetCuree'tTreshold...
+}
