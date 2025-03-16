7833d7e8b397aadf771371fcd7c38cfc37d546dc
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-03-16 14:10:56 +0100

    Create NoEntropyModelVPN.h

---
+++ NoEntropyModelVPN.h
@@ -0,0 +1,42 @@
+//may be wrong. 
+
+//since we don't use any entropy model, we can enqueue features that doesn't have any probability distribution related to mixed services.
+//Now services can run on a multiplateform cpus. So it means that we just have to optimize the convolutional network.
+//To do that, let's enqueue a fourier transform. As we used physics again, let's dont keep attention on signals !
+
+
+classs FourierMessagesVPN  //The usage of entropies is not required but we don't have limits here since we communicate with layers
+{
+
+private:
+   double mainTresholdFourierPhysics = ... : //dont forget to not mix this class with previous classes. So, this treshold can be used in the main function.
+                                            //so, it seems VPN attack. But we don't have any knowledge of the running services on both side. So, keep no using entropies !
+                                            /*
+                                              int main()
+                                              {
+                                                    //if distribution match entropy
+                                                    //return 0;
+
+
+                                              }
+
+                                            */
+}
+
+class NoSignalCPUsEnqueueMainMessagesModel : public MainFeaturesMatcher
+{
+
+
+};
+
+
+class KnowledgeFullStack : public StackDevice
+{
+private:
+
+public:
+        //if we keep an eye on computer graphics, we can see that some materials are not needed. As I said in another repository.
+      //it let us to check errors.
+      void checkErrorsStackEntropyModel(..);  //choose a model. Pathc it. Do whatever you want with your own model.
+};
+
